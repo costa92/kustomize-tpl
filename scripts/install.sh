65746212b52efeebe7356a26ba7701142f118841
costa92 <944310536@qq.com>
2023-12-24 23:47:59 +0800

    fix: add platform

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,1 +1,1 @@
-curl -Lo ./kustomize https://github.com/kubernetes-sigs/kustomize/releases/download/v3.2.0/kustomize_3.2.0_linux_amd64 && sudo chmod +x ./kustomize && sudo mv kustomize /usr/local/bin
+curl -Lo https://github.com/kubernetes-sigs/kustomize/releases/download/v3.2.0/kustomize_3.2.0_linux_amd64 && sudo chmod +x ./kustomize && sudo mv kustomize /usr/local/bin
